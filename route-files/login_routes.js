29902aa4d2be5337c48673d3498564dc89b5d8ba
patsutunya <41244173+patsutunya@users.noreply.github.com>
2023-02-10 18:26:59 -0500

    Add files via upload

---
+++ route-files/login_routes.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const {retrieveUserName} = require('../dao-files/customer_dao')
+const jwt = require('../utility/jwts');
+
+
+router.get('/login', async (req, res) => {
+
+
+ try {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    let data = await retrieveUserName(username);
+    const userName = data.Item
+    if (!userName){
+
+        res.statusCode = 400;
+        res.send({'message': 'Invalid username'})
+    }
+
+    const isValid = await bcrypt.compare(password, userName.password)
+
+    if (!isValid) {
+
+        res.statusCode = 401;
+        res.send({'message': 'Invalid password'})
+  
+    } else {
+
+        res.statusCode = 200;
+       
+        res.send({'message': 'You are successfully login',
+        "token": jwt.newToken(userName.username, userName.role)  
+    });
+        
+   
+}
+
+
+} catch (err){
+
+    res.statusCode = 500;
+    res.send({'message': `${err}`})
+}
+
+});
+
+
+module.exports = router;
