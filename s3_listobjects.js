e606df9f38ca3b3a58fab2286adccbec1d008f64
Victor Cizek <victorjcizek@gmail.com>
2023-02-13 01:23:56 -0500

    added file for retrieving image S3 URLs

---
+++ s3_listobjects.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: 'us-east-1'
+});
+
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+/*const params = {Bucket: 'alchemy-books', Key: 'cover1.jpg'};
+
+function getCoverArt() {
+    console.log(s3.getSignedUrl('getObject', params, {
+        expiresIn: 800
+    }));
+}*/
+
+function getCoverArt() {
+    console.log(s3.getSignedUrl('getObject', {Bucket: 'alchemy-books', Key: 'cover7.jpg', Expires: 604800 }));
+}
+
+getCoverArt();
+
+//to run: node s3_listobjects.js
+
+// Create the parameters for calling listObjects
+/*const bucketParams = {
+  Bucket : 'alchemy-books',
+};*/
+
+// Call S3 to obtain a list of the objects in the bucket
+/*function getCoverArt() {
+    return s3.listObjects(bucketParams).promise();
+}*/
