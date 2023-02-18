9a705c52f8433bfcfc623ff615f4267ff28e2cda
CodeVJC <113875215+CodeVJC@users.noreply.github.com>
2023-02-18 16:04:19 -0500

    Delete fillleesss directory

--- fillleesss/errors/password-matching-error.js
+++
@@ -1,6 +0,0 @@
-module.exports = class PasswordMatchingError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "PasswordMatchingError";
-    }
-}
--- fillleesss/errors/username-already-taken-error.js
+++
@@ -1,6 +0,0 @@
-module.exports = class UsernameAlreadyTakenError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "UsernameAlreadyTakenError";
-    }
-}
